b21710918d05a0599f8a06314471b1ad6b7d36b7
Hwanhee Kim <khhandrea@gmail.com>
2023-12-15 14:34:32 +0900

    fix: Fix threading bug

--- program/ProgramExample.py
+++ program/ProgramExample.py
@@ -1,15 +1,21 @@
+import sys, os
+ 
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
 import MQTTclient
 
 from queue import Queue
-from multiprocessing import Process
+from threading import Thread
 from program import Program
+import time
 
 
 """
 
 고쳐야할 것:
+  - ProgramExample - start
+  - config
   - topic_dispathcer
-  - handler
 
 
 def callback_example(topic, data, publisher):
@@ -20,9 +26,25 @@ def callback_example(topic, data, publisher):
 
 """
 
+class ProgramExample(Program):
+    def __init__(self, config, topic_dispatcher):
+        self.config = config
+        self.topic_dispatcher = topic_dispatcher
+
+        super().__init__(self.config, self.topic_dispatcher)
+
+    # TODO 각자에 맞게 고치면 됨
+    def start(self):
+        while True:
+            self.publisher.publish("hardware/sensors/loopcoil", "ji")
+            time.sleep(1)
+
+    
+# TODO 각자에 맞게 추가하면 됨
 def handle_loopcoil(topic, data, publisher):
     print(f"topic: {topic}")
     print(f"data: {data}")
+    publisher.publish("hihihi", "hihi")
 
 def handle_crossinggate(topic, data, publisher):
     print(f"topic: {topic}")
@@ -30,25 +52,26 @@ def handle_crossinggate(topic, data, publisher):
 
 if __name__ == '__main__':
 
+
+    # TODO 각자에 맞게 고치면 됨
     config = {
-        "ip": "127.0.0.1", 
-        "port": 6007, 
-        "topics": ["hardware/sensors/loopcoil", "hardware/crossinggate"],
+            "ip": "127.0.0.1", 
+            "port": 1883, 
+            "topics": [ # (topic, qos) 순으로 넣으면 subcribe됨
+                ("hardware/sensors/loopcoil", 0), 
+                ("hardware/crossinggate", 0)
+            ],
         }
     
+    # TODO 각자에 맞게 고치면 됨
+    # topic: handler 순으로 추가하면 된다.
+    # 원하는 topic에 해당하는 반응을 구현하면 됨
     topic_dispatcher = {
         "hardware/sensors/loopcoil": handle_loopcoil,
         "hardware/crossinggate": handle_crossinggate,
     }
 
-    example_program = Program
-
+    ex = ProgramExample(config=config, topic_dispatcher=topic_dispatcher)
+    ex.start()
     
 
-    queue = Queue()
-
-    program = Program(config=config)
-
-
-
-    
